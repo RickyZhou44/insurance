f10efa7f7c84ab5a4b921a95bbca28c783b0f19b
RickZhou <819894286@qq.com>
2019-02-27 21:41:40 +0800

    first commit

--- src/main/java/whu/iss/insurance/InsuranceApplication.java
+++ src/main/java/whu/iss/insurance/InsuranceApplication.java
@@ -2,8 +2,10 @@ package whu.iss.insurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class InsuranceApplication {
 
     public static void main(String[] args) {
--- src/main/java/whu/iss/insurance/controller/HelloController.java
+++ src/main/java/whu/iss/insurance/controller/HelloController.java
@@ -1,12 +1,15 @@
 package whu.iss.insurance.controller;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class HelloController {
-    @RequestMapping("hello")
+    @RequestMapping("/hello")
     public String index(){
-        return "hello my name is rick";
+        return "hello my name is rick hhh";
     }
 }
