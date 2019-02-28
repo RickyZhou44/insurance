992c3dc963242784b96822bb642c1a2c1e78398f
RickZhou <819894286@qq.com>
2019-02-28 15:32:57 +0800

    create dto class Result

--- src/main/java/whu/iss/insurance/InsuranceApplication.java
+++ src/main/java/whu/iss/insurance/InsuranceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//
+//整个项目的入口
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class InsuranceApplication {
 
