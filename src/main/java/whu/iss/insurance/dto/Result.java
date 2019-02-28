f77d802b0769fe48bcb5e051029012cc827f32de
RickZhou <819894286@qq.com>
2019-02-28 15:14:56 +0800

    create dto class Result

--- src/main/java/whu/iss/insurance/dto/Result.java
+++ src/main/java/whu/iss/insurance/dto/Result.java
@@ -7,4 +7,31 @@ package whu.iss.insurance.dto;
  * 对传输的数据进行封装
  */
 public class Result {
+    private boolean result;
+    private Object data;
+    private String reason;
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 }
